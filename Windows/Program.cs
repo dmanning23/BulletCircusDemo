cf57dce1267d7da0f4f34ef6a8241d523d413e1b
unknown <dmanning23@hotmail.com>
2014-02-20 20:54:51 -0600

    big check in of all the windows project

---
+++ Windows/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace BulletFlockDemo.Windows
+{
+	static class Program
+	{
+		private static Game1 game;
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			game = new Game1();
+			game.Run();
+		}
+	}
+}
